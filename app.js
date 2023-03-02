8dc1c754728b80ba062ef26091e54f4b4d59371a
Dengxian ji <99891555+jadenjjjj@users.noreply.github.com>
2023-03-02 16:56:36 +0000

    fix database error

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
+const express =  require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const indexRoutes = require("./routes/index.routes");
+const MONGO_URI = require('./db');
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
 require("dotenv").config();
 
 // ℹ️ Connects to the database
-require("./db");
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require("express");
+ require("./db/index");
 
 const app = express();
+// const PORT = process.env.PORT || 4000;
+
+// Set up CORS middleware
+const allowedOrigins = [process.env.ORIGIN];
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions));
+
+//Connect to MongoDB
+const uri = process.env.MONGO_URI;
+console.log("uri:", uri);
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // createIndexes: true,
+})
+.then(() => {
+    console.log("MongoDB database connection established successfully");
+})
+.catch((error) => {
+    console.log(error);
+})
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+const mangaRouter = require('./routes/manga')
+app.use("/api/manga", mangaRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+// app.listen(PORT, () => {
+//     console.log(`Server started at http://localhost:${PORT} `)
+// })
 
 module.exports = app;
--- db/index.js
+++ db/index.js
@@ -8,12 +8,4 @@ const mongoose = require("mongoose");
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/kenBackend";
 
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    const dbName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${dbName}"`);
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+module.exports = MONGO_URI;
